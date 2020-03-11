ee18dc8e362c7b1fb3eea4502ef2994fb6ebfada
Brian <g00362192@gmit.ie>
2020-03-11 16:54:42 +0000

    Death/Heatlh prototype

---
+++ Professional-Practice/Assets/Scripts/Player Health/Player_Health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_Health : MonoBehaviour
+{
+    public int health;
+    public bool hasDied;
+
+    void Start() {
+        hasDied = false;
+    }
+
+    void Update() {
+        if (gameObject.transform.position.y < -7) {
+            hasDied = true;
+        }
+        if (hasDied == true) {
+            StartCoroutine("Die");
+        }
+    }
+
+    IEnumerator Die () {
+        SceneManager.LoadScene("Main");
+        yield return null;
+    }
+
+
+}
+
+   /* void Die()
+    {
+        SceneManager.LoadScene("Prototype_1");
+    } */
